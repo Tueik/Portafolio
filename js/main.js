fef395fc44e0fa43ab11be5a1f99a213a3b283a7
Tueik <ricardoespartan2013@gmail.com>
2022-08-03 00:52:11 -0500

    Updates al portafolio | Social Media, Img Background

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// // Una alerta es una ventana de dialogo que muestra un mensaje
+// alert ("Bienvenido");
+
+// // Pedimos la información al usuario con un prompt, la guardamos en una variable llamada nombre para después mostrarla
+// nombre = prompt("Introduce tu nickname");
+
+// // usamos un console.log para mostrar lo que se guardó en la variable nombre
+// console.log(nombre);
+
